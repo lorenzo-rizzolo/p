a85d78ee1dddeaeaa567fdc637e9405181a8c5f8
lorenzo-rizzolo <83411201+lorenzo-rizzolo@users.noreply.github.com>
2021-12-28 00:41:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var utente=document.getElementById('utente').value;
 
 console.log(pass)
 if(pass=="1209" && utente=="lorenzo"){   //password
-    window.open("2_pag/program.html") //file
+    window.open("2_pag/l_c.html") //file
 }
 else{
     alert("password errata");
 }
 if(pass=="alberto123" && utente=="alberto"){   //password
-    window.open("2_pag/program.html") //file
+    window.open("2_pag/l_c.html") //file
 }
 else{
     alert("password errata");
