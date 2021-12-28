cd0cc5bbd56bbc02fa5cb2d31944ab0f753e9912
lorenzo-rizzolo <83411201+lorenzo-rizzolo@users.noreply.github.com>
2021-12-28 15:18:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ function AreaProtetta(){
 var pass=document.getElementById('testo').value;
 var utente=document.getElementById('utente').value;
 
+
 console.log(pass)
-if(pass=="1209" && utente=="lorenzo"){   //password
-    window.open("2_pag/l_c.html") //file
+
+if(
+pass=="1209" && utente=="lorenzo" 
+|| pass=="alberto123" && utente=="alberto"
+){   //password
+    window.open("2_pag/program.html") //file
+    
 }
 else{
-    alert("password errata");
+    alert("password o nome utente errato")
 }
-if(pass=="alberto123" && utente=="alberto"){   //password
-    window.open("2_pag/l_c.html") //file
-}
-else{
-    alert("password errata");
-}
+
+
 }
